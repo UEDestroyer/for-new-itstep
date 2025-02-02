01528f2cde2f289bc609d35d000e064f916b15d4
ӘД <163650738+UEDestroyer@users.noreply.github.com>
2025-02-02 16:34:56 +0500

    Create p.bat

---
+++ p.bat
@@ -0,0 +1,18 @@
+@echo off
+setlocal EnableDelayedExpansion
+
+set "MSYS2_PATH=C:\msys64\usr\bin\bash.exe"
+set "COMMAND="
+set CURRENT_DIR=%cd%
+
+if "%1"=="" (
+    set /p "COMMAND=Enter command: "
+) else (
+    set "COMMAND=%*"
+)
+
+set "COMMAND=!COMMAND:.\=./!"
+
+set "COMMAND=!COMMAND:&&=;!"
+
+"%MSYS2_PATH%" -l -c "cd '%CURRENT_DIR%' && %COMMAND%"
